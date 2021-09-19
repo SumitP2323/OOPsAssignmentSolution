08aaa0270eb4f2489c6153c0286e8f1832d3244d
SumitP2323 <sumitpatel2323@gmail.com>
2021-09-19 22:28:52 +0530

    commit

---
+++ OopsAssignment/src/oopsAssignment/SuperDepartment.java
@@ -0,0 +1,18 @@
+package oopsAssignment;
+
+public class SuperDepartment {
+	public String deparmentName() {
+		return " Super Deparment  ";
+	}
+	public String getTodaysWork() {
+		return " No work as of now ";
+	}
+	public String getWorkDeadline() {
+		return " Nil ";
+	}
+	 
+	public String isTodayAHoliday() {
+		return "Today is not a Holiday";
+	}
+
+}
---
+++ OopsAssignment/src/oopsAssignment/TechDepartment.java
@@ -0,0 +1,20 @@
+package oopsAssignment;
+
+public class TechDepartment extends SuperDepartment{
+
+	public String deparmentName() {
+		return " Welcome to Tech Department";
+	}
+
+	public String getTodaysWork() {
+		return "Complete coding of Module 1";
+	}
+	public String getTechStackInformation() {
+		return " Core Java ";
+	}
+
+	public String getWorkDeadline() {
+		return "Complete by EOD ";
+	}
+
+}
